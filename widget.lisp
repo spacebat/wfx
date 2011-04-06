0980aa3734cb9954559b081f111da687d47bc13e
Stas Boukarev <stassats@gmail.com>
2011-04-06 14:57:56 +0400

    make-widget: Use the name of widget-class if name isn't provided.

--- widget.lisp
+++ widget.lisp
@@ -105,7 +105,11 @@
   "This function instanciates a widget or returns the widget from the dom if it already exists.
 Each request uri has its own hashtable with widgets. The hashtable represents a simple dom.
 The dom is automatically updated before a request is passed to a hunchentoot handler."
-  (let* ((cache (cache))
+  (let* ((name (or name (class-name
+                         (etypecase widget-class
+                           (symbol (find-class widget-class))
+                           (class widget-class)))))
+         (cache (cache))
          (instance (gethash name cache)))
     (cond ((not instance)
            (setf instance (apply #'make-instance widget-class args))
